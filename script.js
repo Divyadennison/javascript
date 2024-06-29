0cf2308de102699ab11466556903d779cf4afb10
Divyadennison <divyadennison@gmail.com>
2024-06-29 09:54:32 +0530

    Array

--- script.js
+++ script.js
@@ -1,38 +1,113 @@
-console.log("HELLO WORLD")
-console.log("hello world");
-var a=10;
-console.log(a);
-//var a="Name"
- //console.log(a);
+// console.log("HELLO WORLD")
+// console.log("hello world");
+// var a=10;
+// console.log(a);
+// //var a="Name"
+//  //console.log(a);
 
 
-//let name="John";
-// // //console.log(name);
-// // // let name
+// //let name="John";
+// // // //console.log(name);
+// // // // let name
 
 
-const pi=3.14;
- console.log(pi);
-// const pi=3.145
+// const pi=3.14;
+//  console.log(pi);
+// // const pi=3.145
  
 
-// // // Nan
+// // // // Nan
 
-let valueOne=2;
-let valueTwo="a";
- console.log(valueOne+valueTwo);
- console.log(valueOne+parseInt(valueTwo));
-
-
-
- const name="John";
-  const age=30;
- const rating=4.5;
- const isCool=true;
- const x=null;
- var y;
-
-console.log(typeof name);
-console.log('My name is ' + name + ' and I am '+age);
+// let valueOne=2;
+// let valueTwo="a";
+//  console.log(valueOne+valueTwo);
+//  console.log(valueOne+parseInt(valueTwo));
 
 
+
+//  const name="John";
+//   const age=30;
+//  const rating=4.5;
+//  const isCool=true;
+//  const x=null;
+//  var y;
+
+// console.log(typeof name);
+// console.log('My name is ' + name + ' and I am '+age);
+
+
+
+
+// // //Array
+
+ arrayOne=["john",30,true,null,undefined]
+ console.log(arrayOne);
+
+// // // //push
+ arrayOne.push("Apple");
+ console.log(arrayOne);
+
+// // // //pop
+  arrayOne.pop();
+  console.log(arrayOne);
+// // // //unshift
+  arrayOne.unshift("Mango");
+  console.log(arrayOne);
+
+ console.log(arrayOne[1]);
+  arrayOne[3]="banana"
+  console.log(arrayOne);
+
+  console.log(arrayOne.length);
+
+
+arrayOne.splice(3,1);
+ console.log(arrayOne);
+
+
+
+  let valueOnee="2"
+ let valueTwoo=2;
+ console.log(valueOnee==valueTwoo);
+
+  console.log(valueOnee === valueTwoo);
+
+
+// // // //Conditional statements
+// // // //if
+  let value =15;
+  if(value >=18)
+       {
+           console.log("You are Adult")
+              }
+
+//  let valuee =18;
+
+//  if (valuee >=18)
+//     {
+
+    
+//     console.log("You are adult");
+//     }
+
+
+
+
+// //     //if else
+   let valuee =16;
+      if(valuee >=18)
+       {
+          console.log("You are Adult");
+     }
+
+   else
+    {
+                  console.log("You are a minor");
+
+   }
+
+// //     //ternary
+    let age=20;
+    let type=(age >=18) ? "Adult" : "Minor"
+   console.log(type);
+    
